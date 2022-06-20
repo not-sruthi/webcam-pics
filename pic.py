79915b97ad1244382305fb58c59dab6b56e7b78d
not-sruthi <75844345+not-sruthi@users.noreply.github.com>
2022-06-20 23:26:15 +0530

    Update pic.py

--- pic.py
+++ pic.py
@@ -2,8 +2,10 @@ import time
 import cv2
 import dropbox
 
+
 startTime = time.time()
 
+#to take pictures from webcam and saving them
 def takePic(n):
     videoCaptureObject = cv2.VideoCapture(0)
     result = True
@@ -18,7 +20,7 @@ def takePic(n):
     cv2.destroyAllWindows
     return(img)
 
-
+#to upload pictures to dropbox
 def uploadPic(img):
     accessToken= 'alJYiWd_lRYAAAAAAAAAATCaNHtOfk6FzZVQUgSQceeqe133912Dt5mbUHXrEFf4'
     file= img
@@ -30,6 +32,7 @@ def uploadPic(img):
         db.files_upload(f.read(), fileTo, mode = dropbox.files.WriteMode.overwrite)
         print('success')
 
+#taking pictures at 5 second intervals and uploading them to dropbox
 def main():
     isRunning = True   
     no = 0
